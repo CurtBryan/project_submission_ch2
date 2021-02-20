da6f116035e8a4e88d9d2578d814d96c440a3363
Curt Bryan <curtbryan3rd@gmail.com>
2021-02-20 14:57:50 -0700

    Finished endpoint for filtering the image to match requirements

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {getFilteredImage} from './Controllers/getFilteredImage'
 
 (async () => {
 
@@ -17,14 +17,13 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
-  // RETURNS
+  // RETURNS 
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
@@ -36,7 +35,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
+  app.get("/filteredimage", getFilteredImage)
 
   // Start the Server
   app.listen( port, () => {
